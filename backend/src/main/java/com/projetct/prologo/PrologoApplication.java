0574449c6e75f453fdf471072f8b6f569f99e697
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2022-09-07 20:37:09 -0300

    Project created

---
+++ backend/src/main/java/com/projetct/prologo/PrologoApplication.java
@@ -0,0 +1,13 @@
+package com.projetct.prologo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrologoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrologoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetct/prologo/PrologoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetct.prologo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrologoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
