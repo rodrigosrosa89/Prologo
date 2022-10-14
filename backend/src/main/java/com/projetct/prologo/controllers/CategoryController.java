977349bfa95f4e42abb1232bcf9d209386b2a0a3
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2022-10-14 08:03:59 -0300

    Axios implementation

--- backend/src/main/java/com/projetct/prologo/controllers/CategoryController.java
+++ backend/src/main/java/com/projetct/prologo/controllers/CategoryController.java
@@ -1,12 +1,7 @@
 package com.projetct.prologo.controllers;
 
-import com.projetct.prologo.entities.Sale;
-import com.projetct.prologo.services.SaleService;
-import com.projetct.prologo.services.SmsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/category")
