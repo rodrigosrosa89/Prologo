9ae2c8279ae22b26f0c3d486a4f6f3b232cce4f4
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2022-10-14 01:04:05 -0300

    Database

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -16,7 +16,7 @@ function SalesCard() {
 
     return (
         <div className="dsmeta-card">
-            <h2 className="dsmeta-sales-title">Produtos</h2>
+            <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
                     <DatePicker
