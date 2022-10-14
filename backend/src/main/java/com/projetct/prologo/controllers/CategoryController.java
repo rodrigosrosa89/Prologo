1fd15c1ecc35e7cf3bb042bd5684794280716765
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2022-10-14 02:50:01 -0300

    create product and category controllers

---
+++ backend/src/main/java/com/projetct/prologo/controllers/CategoryController.java
@@ -0,0 +1,15 @@
+package com.projetct.prologo.controllers;
+
+import com.projetct.prologo.entities.Sale;
+import com.projetct.prologo.services.SaleService;
+import com.projetct.prologo.services.SmsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/category")
+public class CategoryController {
+
+}
