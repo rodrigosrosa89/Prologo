d8c746b01ddf385bcd5d22632b8af44aa0dedd02
Rodrigo Rosa <rodsiqueirabh@hotmail.com>
2022-10-15 23:30:25 -0300

    fix deploy site3

---
+++ frontend/src/components/layout/container/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css'
+import { Link } from 'react-router-dom'
+
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+                <ul className='list'>
+                    <li className='item'><Link to="/">Home</Link></li>
+                    <li className='item'><Link to="/product">Produtos</Link></li>
+                    <li className='item'><Link to="/category">Categorias</Link></li>
+                    <li className='item'><Link to="/sales">Vendas</Link></li>
+                </ul>
+        </nav>
+    )
+}
+
+export default Navbar
--- frontend/src/components/layout/navbar/index.tsx
+++ frontend/src/components/layout/navbar/index.tsx
@@ -1,18 +1,9 @@
 import './styles.css'
-import { Link } from 'react-router-dom'
 
 
-function Navbar() {
+function Container() {
     return (
-        <nav className='navbar'>
-                <ul className='list'>
-                    <li className='item'><Link to="/">Home</Link></li>
-                    <li className='item'><Link to="/product">Produtos</Link></li>
-                    <li className='item'><Link to="/category">Categorias</Link></li>
-                    <li className='item'><Link to="/sales">Vendas</Link></li>
-                </ul>
-        </nav>
     )
 }
 
-export default Navbar
+export default Container
